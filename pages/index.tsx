0206c9b0312754a008c9ce09d26161fb3ab4cc58
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2022-05-28 13:45:42 +0200

    Sanity setup + Tweet components + loading the sanity info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+const Home = ({ tweets }: Props) => {
+  console.log(tweets)
   return (
     <div className="mx-h-screen mx-auto overflow-hidden lg:max-w-6xl">
       <Head>
@@ -14,7 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="grid grid-cols-9">
         <Sidebar />
-        <Feed />
+        <Feed tweets={tweets} />
         <Widgets />
       </main>
     </div>
@@ -22,3 +28,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets()
+  console.log(`tweets: ${tweets}`)
+  return {
+    props: {
+      tweets,
+    },
+  }
+}
